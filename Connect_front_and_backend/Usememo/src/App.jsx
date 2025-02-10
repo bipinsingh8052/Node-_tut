58c388701827785ef358c7d859589fb992445314
Bipin Singh <bipinsingh8052@gmail.com>
2025-02-10 16:16:56 +0530

    useMemo and callback Hook

---
+++ Connect_front_and_backend/Usememo/src/App.jsx
@@ -0,0 +1,61 @@
+import { useMemo } from "react";
+import { useState } from "react"
+
+
+function App() {
+ let[add,setAdd]=useState(0);
+ let [sub,Setsub]=useState(100);
+ let[no,setno]=useState(0);
+ let[city,setcity]=useState("");
+ const multip=useMemo(()=>{
+  console.log("memo value");
+  return add*2;
+ },[add])
+
+
+
+
+
+
+
+
+
+ const memorize =useMemo(()=>{
+  for(let i=0;i<=1000000000;i++){}
+  return no*2
+ },[no])
+  return (
+    <>
+     <h1>This is MemoValue :{multip}</h1>
+     <h1>This is Counter:{add}</h1>
+     <button onClick={()=>{setAdd(add+1)}}>Add</button>
+     <h1>THis is Substracr:{sub}</h1>
+     <button onClick={()=>{Setsub(sub-1)}}>Sub</button>
+
+
+
+
+
+     <br />
+
+     <br /><br /><br /><br /><br /><br /><br /><br /><br />
+     <hr />
+     Enter the number: <input type="number" value={no} onChange={(e)=>{setno(e.target.value)}}/>
+     Enter the city: <input type="text" value={city} onChange={(e)=>{setcity(e.target.value)}}/>
+
+
+     <h1>memorize value:{memorize}</h1>
+
+
+
+
+
+
+
+
+     
+    </>
+  )
+}
+
+export default App
---
+++ Connect_front_and_backend/Usememo/src/Parent.jsx
@@ -0,0 +1,13 @@
+import React, { memo, useState } from 'react'
+
+function Parent() {
+    let[task,setTask]=useState([]);
+    let[count,setcount]=useState("");
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+export default memo(Parent)
