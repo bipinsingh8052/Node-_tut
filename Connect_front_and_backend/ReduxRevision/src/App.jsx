4aa07857d65f7bdcd62cdff1338697bfbfb5edf9
Bipin Singh <bipinsingh8052@gmail.com>
2025-02-13 17:33:56 +0530

    todo app completed from redux toolkit

--- Connect_front_and_backend/ReduxRevision/src/App.jsx
+++ Connect_front_and_backend/ReduxRevision/src/App.jsx
@@ -1,25 +1,32 @@
 import { useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { mycolorChange } from "./ColorSlice";
-import { addtodo,mydelet } from "./TodoSlice";
+import { addtodo,mydelet ,InCompleted,Completed,editdata} from "./TodoSlice";
 function App() {
   let [input,setinput]=useState("")
 
   let [todoinput,settodoiinput]=useState("");
+  const [storeId ,setStoreId]=useState("");
+  const [addEditbtn,setAddEditbtn]=useState(false)
   const displ =useDispatch();
+
   let redi=useSelector((state)=>state.color.mycolor)
 
 
   let todolist =useSelector(state=>state.todo.todolist)
   console.log(todolist)
  
-
+const editTodo=(id,data)=>{
+setStoreId(id)
+settodoiinput(data)
+setAddEditbtn(true)
+}
   return (
     <>
 
     <h1>This is my color changer app</h1>
     <input type="text"  onChange={(e)=>{setinput(e.target.value)}} />
-    <button onClick={()=>{displ(mycolorChange(input))}}>change it color</button>
+   <button onClick={()=>{displ(mycolorChange(input))}}>change it color</button>
     <div style={{height:"200px", width:"200px", border:"2px solid black", backgroundColor:redi}}></div>
 
 
@@ -27,7 +34,11 @@ function App() {
     <center>
       <h1>TOdo list in this Here</h1>
       <input type="text" value={todoinput} onChange={(e)=>{settodoiinput(e.target.value)}} />
-      <button onClick={()=>{ displ(addtodo({work:todoinput,status:false,id:Date.now()}))}}>Add todo</button>
+      {
+        addEditbtn?(<button onClick={()=>{displ(editdata({id:storeId,input:todoinput})),setAddEditbtn(false)}}>edit</button>):
+        (<button onClick={()=>{displ(addtodo({id:Date.now(),work:todoinput,status:false}))}}>Add todo</button>)
+      }
+      
       <hr />
       
       <table>
@@ -44,8 +55,18 @@ function App() {
               return(
                 <tr key={index}>
                   <td>{index+1}</td>
-                  <td>{e.work}</td>
+                  <td>
+                    {e.status?(<span style={{color:"red", textDecoration:"line-through"}}>{e.work}</span>):(<span>{e.work}</span>)
+                    }
+                  </td>
                   <td><button onClick={()=>{displ(mydelet(e.id))}}>Dele</button></td>
+                  <td>
+                    { e.status?(<button onClick={()=>{displ(InCompleted({id:e.id}))}}>InCompleted</button>):(<button onClick={()=>{displ(Completed({id:e.id}))}}>Completed</button>)
+                    }</td>
+
+                    <td>
+                      <button onClick={()=>{editTodo(e.id,e.work)}}>edit</button>
+                    </td>
                 </tr>
               )
             })
--- Connect_front_and_backend/ReduxRevision/src/TodoSlice.jsx
+++ Connect_front_and_backend/ReduxRevision/src/TodoSlice.jsx
@@ -13,9 +13,32 @@ const TodoSlice =createSlice({
         mydelet:(state,actions)=>{
             console.log(actions.payload)
             state.todolist=state.todolist.filter(e=>e.id!==actions.payload)
+        },
+        InCompleted:(state,actions)=>{
+            for(let i=0;i<state.todolist.length;i++){
+                if(state.todolist[i].id=== actions.payload.id){
+                    state.todolist[i].status=false
+                }
+            }
+        },
+        Completed:(state,actions)=>{
+            for(let i=0;i<state.todolist.length;i++){
+                if(state.todolist[i].id=== actions.payload.id){
+                    state.todolist[i].status=true
+                }
+            }
+        },
+        editdata:(state,actions)=>{
+            console.log(actions.payload)
+            for(let i=0;i<state.todolist.length;i++){
+                if(state.todolist[i].id=== actions.payload.id){
+                    state.todolist[i].work=actions.payload.input;
+                }
+            }
         }
+
     }
 })
 
-export const {addtodo,mydelet} =TodoSlice.actions
+export const {addtodo,mydelet,InCompleted,Completed,editdata} =TodoSlice.actions
 export default TodoSlice.reducer
