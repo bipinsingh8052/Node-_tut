62d4e16d2657549d37a5c6f2bded341304322905
Bipin Singh <bipinsingh8052@gmail.com>
2025-02-18 10:31:52 +0530

    working

--- LoginSignup/frontend/src/Components/Login.jsx
+++ LoginSignup/frontend/src/Components/Login.jsx
@@ -3,34 +3,44 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import {message} from 'antd'
 import axios from 'axios'
+
+
+
+
+
+// npm install react@18 react-dom@18
 export default function Login() {
   let[input,setinput]=useState({});
   const handleinput=(e)=>{
     let {name,value}=e.target;
     setinput(values=>({...values,[name]:value}))
   }
-  message.success("tyuiopjb")
   // alert("tyuhijkl")
   const submitfrom = async(e)=>{
     e.preventDefault();
     console.log(input)
     try {
       let api ="http://localhost:8080/info/login";
-     let res = axios.post(api,input)
+     let res = await axios.post(api,input)
       console.log(res);
      
      
 
       // console.log(res.data);
       // // alert(res.data.msg)
+      message.success(res.data.msg)
       // console.log(res.response.data.msg)
     } catch (error) {
       
       // console.log("error",error.response.data.msg)
 // alert(error.response.data.msg)
-      console.log(error.response.data.msg)
+      // console.log(error.response.data.msg)
       // console.log(res.response.data.msg)
       // alert(res.response.data.msg)
+
+      // console.log("Error:", error.response.data.msg );
+      message.error(error.response.data.msg);
+      // alert(error.response.data.msg)
       
     }
     
@@ -42,6 +52,7 @@ export default function Login() {
   
   return (
     <>
+      {/* <ConfigProvider theme={{ token: {} }}> */}
        <Form >
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
@@ -57,6 +68,7 @@ export default function Login() {
         Submit
       </Button>
     </Form>
+    {/* </ConfigProvider> */}
     </>
   )
 }
