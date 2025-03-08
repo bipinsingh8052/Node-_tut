4c15e118e21b74e1e1967d4c2444819aec429e3d
Bipin Singh <bipinsingh8052@gmail.com>
2025-03-08 16:16:27 +0530

    cookiess and session

--- ONeToManyRetationShip/server/index.js
+++ ONeToManyRetationShip/server/index.js
@@ -4,12 +4,12 @@ const mongoose=require("mongoose");
 const  bodyParser =require("body-parser");
 const cors =require("cors");
 require("dotenv").config();
-
+const cookies =require("cookie-parser")
 const Router =require("./Routes/Route")
 
+var session = require('express-session')
 
-
-
+app.use(cookies())
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -22,7 +22,11 @@ mongoose.connect(process.env.DBCONNECTED).then(()=>{console.log("Db is Connected
 .catch(()=>{
     console.log("NOt conneccted the DB")
 })
-
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+  }))
 
 
 
@@ -30,6 +34,40 @@ mongoose.connect(process.env.DBCONNECTED).then(()=>{console.log("Db is Connected
 let port =process.env.PORT||9009;
 app.use("/author",Router)
 
+app.get("/setCookies",(req,res)=>{
+    res.cookie("studentname","Bipin singh")
+    res.cookie("student","Bipin singh")
+    res.cookie("name","Bipin singh")
+    res.cookie("normal","Bipin singh")
+    res.send("Okk this is Send the cookies in")
+})
+
+app.get("/getcooki" ,(req,res)=>{
+    // res.cookie("studentname");
+    // let a =req.cookies("studentname")
+    console.log(res.cookies)
+    res.send(req.cookies)
+})
+
+app.get("/deletecokki",(req,res)=>{
+    console.log("okk delete");
+    res.clearCookie("normal");
+    res.clearCookie("name");
+    res.send(req.cookies);
+})
+
+app.get("/setSession",(req,res)=>{
+    console.log("okk session");
+    req.session.myname="bipin singh yadav",
+    req.session.myclass="bsc ";
+    res.send("okk send the session")
+})
+
+
+app.get("/getsession",(req,res)=>{
+    console.log("this is get sessoion");
+    res.send(req.session)
+})
 app.listen(port,()=>{
     console.log(`http://localhost:${port}`);
 })
